cecc605915ae9079782e53cc136da09b69079f49
saroshkhann <ksarosh785@gmail.com>
2024-08-21 12:59:15 +0500

    modified 🚀

--- script.js
+++ script.js
@@ -2,17 +2,33 @@
 
 const container = document.querySelector('.container');
 const calcInput = document.querySelector('.calc__input');
+const btnAC = document.querySelector('.AC');
 
 container.addEventListener('click', function (e) {
-  if (e.target.nodeName == 'BUTTON') {
-    calcInput.value = calcInput.value + e.target.innerHTML;
-  }
+  const btnValue = e.target.innerHTML;
 
-  if (e.target.innerHTML == 'C') {
-    calcInput.value = '';
-  }
+  console.log(btnValue);
 
-  if (e.target.innerHTML == '=') {
-    eval(calcInput.value);
+  if (e.target.nodeName === 'BUTTON') {
+    btnAC.textContent = 'C';
+
+    if (btnValue === 'C' ?? 'AC') {
+      calcInput.value = '';
+    } else if (btnValue === '=') {
+      try {
+        const expression = calcInput.value
+          .replaceAll('X', '*')
+          .replaceAll('÷', '/');
+        const val = eval(expression);
+
+        calcInput.value = val;
+      } catch {
+        calcInput.value = 'Error';
+      }
+    } else if (btnValue === '%') {
+      calcInput.value = calcInput.value / 100;
+    } else {
+      calcInput.value += btnValue;
+    }
   }
 });
